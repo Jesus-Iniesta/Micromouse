6c4e80eb5c10eeed5804b1ce985d92352ab3ff56
Jesus-Iniesta <iniestavalverdejesus4@gmail.com>
2025-12-15 22:34:37 -0600

    fix: Estructura de proyecto lista

---
+++ MicroMpuse/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+    // Inicialización aquí
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("Robot Laberinto - Inicializando...");
+}
+
+void loop() {
+    // Código principal aquí
+    delay(100);
+}
